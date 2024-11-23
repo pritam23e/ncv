9eed456b963966222220cdd5bdf4fd9cbfdbb8bb
Codecblue <167698377+pritam23e@users.noreply.github.com>
2024-11-23 20:30:58 +0530

    Update finitewell.m

--- hmevprob/finitewell.m
+++ hmevprob/finitewell.m
@@ -1,6 +1,6 @@
+% FINITE WELL
 clear all
 close all
-
 % Parameters
 xvi = -3;         % Initial time
 y_0 = 0;       % Initial value of wave function
@@ -11,23 +11,18 @@ N=1000;    %Number of iterations for Euler
 V0=4;
 xi=xvi-1;
 xf=xvf+1;
-
-
 % Initialize Function
 ddy = @(xl, yl, y1l, El) (-El*yl).*(xl > xvi & xl < xvf) + ((V0 - El)*yl).*(~(xl > xvi & xl < xvf));      
-
 %Eigen value conditions
 dn=0.01;
 Ei=0;
 Ef=3;
 Eo=Ei:dn:Ef;
 k=length(Eo);
-
 %Eigen value finder
 [E,Ev]=bisectHM(k,Eo,xi,y_0,y1_0,xf,ddy,N);
 E
 Ne=length(E)
-
 %Plotter
 for i=1:Ne
     [Y,X]=Rk4HM(xi,xf,y_0,y1_0,ddy,N,E(i));
@@ -36,7 +31,6 @@ for i=1:Ne
     legends{i} = sprintf('%dth excited state, E = %.6f', i-1, E(i));              
     hold on   
 end
-
 subplot(2, 1, 1)
 grid on;
 xlim([-6 6])
@@ -44,11 +38,9 @@ xlabel('x');
 ylabel('y');
 title('Solution of Potential Well');
 legend(legends, 'Location', 'best');
-
-
 subplot(2, 1, 2);
 plot(Eo,Ev)
 grid on
 xlabel('Final value of function  ');
-ylabel('Eneegy values');
+ylabel('EneRgy values');
 title('Eigen Value Finder');
